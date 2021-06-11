c493f6be961c460d90bb0a43fc88b360d5be3406
Rahul Mangalampalli <rahul_mangalampalli@yahoo.in>
2021-06-11 21:49:53 +0530

    updated classes

--- data/config.py
+++ data/config.py
@@ -138,8 +138,8 @@ diamond = dataset_base.copy({
     'valid_info':   '/content/output.json',
 
     'has_gt': True,
-    'class_names':("Crystal", "Feather", "Needle", "Twinning Wisp", "Cloud", "Internal Graining", "Natural", "Knot", "Indented Natural", "Bruise"),
-    'label_map': {0:1,1:2,2:3,3:4,4:5,5:6,6:7,7:8,8:9,9:10}
+    'class_names':("Crystal", "Feather", "Needle", "Twinning Wisp", "Cloud", "Internal Graining", "Natural", "Knot", "Indented Natural", "Bruise","Surface Graining", "Etch Channel"),
+    'label_map': {0:1,1:2,2:3,3:4,4:5,5:6,6:7,7:8,8:9,9:10,10:11,11:12}
 })
 coco2014_dataset = dataset_base.copy({
     'name': 'COCO 2014',
@@ -671,7 +671,7 @@ yolact_base_config = coco_base_config.copy({
 
     # Dataset stuff
     'dataset': diamond,
-    'num_classes': 11,
+    'num_classes': 13,
 
     # Image Size
     'max_size': 550,
